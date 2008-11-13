88758b140d839fd0356ae18144c5e545dde036a6
ronys <ronys>
2008-11-13 21:11:10 +0000

    Bring all code under Artistic Licence 2.0 and note David as the copyright owner.

--- PasswordSafeJ/src/org/pwsafe/passwordsafej/SpringUtilities.java
+++ PasswordSafeJ/src/org/pwsafe/passwordsafej/SpringUtilities.java
@@ -1,5 +1,11 @@
 /*
  * $Id$
+ *
+ * Copyright (c) 2008 David Muller <roxon@users.sourceforge.net>.
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
  * 
  * This file is adapted from a sample taken from the Java Tutorial by Sun.
  */
